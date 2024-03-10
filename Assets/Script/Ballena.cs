a83c3ea5af9ec364647e78300654bd10a187d43a
FranciscoCai <jiehaocai54@gmail.com>
2024-03-10 15:41:04 +0100

    ballena

--- Assets/Script/Ballena.cs
+++ Assets/Script/Ballena.cs
@@ -10,22 +10,25 @@ public class Ballena : Movimiento
 {
     public EstadoBarco Estado;
     [SerializeField] private GameObject ObjetoASeguir;
+    [SerializeField] private float TiempoDeEspera;
+    private bool EstaEsperando = false;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(Estado == EstadoBarco.SeguirBarco)
+        if (Estado == EstadoBarco.SeguirBarco)
         {
             return;
         }
-        else if(collision.gameObject.CompareTag("Boats"))
+        else if (collision.gameObject.CompareTag("Boats"))
         {
             Estado = EstadoBarco.SeguirBarco;
             ObjetoASeguir = collision.gameObject;
+            StartCoroutine(GiroAleatorio());
         }
-        else if(Estado == EstadoBarco.SeguirBallena)
+        else if (Estado == EstadoBarco.SeguirBallena)
         {
             return;
         }
-        else if(collision.gameObject.CompareTag("Whale"))
+        else if (collision.gameObject.CompareTag("Whale"))
         {
             Estado = EstadoBarco.SeguirBallena;
             ObjetoASeguir = collision.gameObject;
@@ -33,18 +36,40 @@ public class Ballena : Movimiento
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if(Estado == EstadoBarco.SeguirBarco)
+        {
+            return;
+        }
         Estado = EstadoBarco.Independiente;
         ObjetoASeguir = null;
     }
     private void Update()
     {
-        if (ObjetoASeguir != null)
+        if (ObjetoASeguir != null && Estado != EstadoBarco.SeguirBarco)
         {
             Vector2 direccion = (Vector2)ObjetoASeguir.transform.position - (Vector2)transform.position;
             direccion.Normalize();
             rb.velocity = direccion * InitialVelocity;
         }
-        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle-180, Vector3.forward);
+        if (EstaEsperando == false)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 180, Vector3.forward);
+        }
+    }
+    private IEnumerator GiroAleatorio()
+    {
+        EstaEsperando = true;
+        rb.velocity = Vector3.zero;
+        yield return new WaitForSeconds(TiempoDeEspera);
+        EstaEsperando = false;
+        float angle = Random.Range(0f, 360f);
+        // Convierte el ¨¢ngulo a radianes
+        float radianAngle = angle * Mathf.Deg2Rad;
+        // Calcula las componentes x e y de la velocidad basadas en el ¨¢ngulo
+        float velocityX = Mathf.Cos(radianAngle) * InitialVelocity;
+        float velocityY = Mathf.Sin(radianAngle) * InitialVelocity;
+        // Aplica la velocidad al Rigidbody2D
+        rb.velocity = new Vector2(velocityX, velocityY);
     }
 }
