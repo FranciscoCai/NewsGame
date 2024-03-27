2ad192c97556f8e6a33ebd5c6e9eb24d943d0e1a
FranciscoCai <jiehaocai54@gmail.com>
2024-03-27 12:58:58 +0100

    meta

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
    
     public string boats;
     public int scoreNeed = 0;
-    private int score = 0;  
+    public int score = 0;  
     public bool winConditionOne = false;
     
     private void OnCollisionEnter2D(Collision2D collision)
@@ -19,13 +20,10 @@ public class Goal : MonoBehaviour
             CameraFollowBoats.instance.targets.Remove(collision.transform);
             Destroy(collision.gameObject);
             score = score + 1;
-        }
-    }
-    public void Update()
-    {
-        if(score == scoreNeed)
-        {
-            winConditionOne = true;
+            if (score <= scoreNeed)
+            {
+                SceneManager.LoadScene("Victory");
+            }
         }
     }
 }
